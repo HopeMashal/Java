57e18beb2bbddec8b53e26eb68e910cd68079fcb
HopeMashal <mashal.hope@gmail.com>
2022-07-11 02:21:03 +0300

    edit readme and lesson1 files

--- Notes/lesson_1.java
+++ Notes/lesson_1.java
@@ -1,8 +1,24 @@
+/*_______________________________*/
+//! Java Notes!! 
+
+// You can use { //... } for inline comments And { /* ... */ } for multiline comments
+// inline comments
+/* 
+multiline comments
+*/
+
+/*_______________________________*/
+
 package Notes; //You need to put folder name like this
 
 // You need to put your code into a { CLASS }
 public class lesson_1 {
   public static void main(String[] args){ //MAIN method is so important to start the code
-    System.out.println("Welcome to Java!!");
+    //To print something use System.out.println("Your_Msg");
+    System.out.println("Welcome to Java!!"); 
   }
 }
+
+// Semicolon (;) SO IMPORTANT!!!!!! Don't forget it :)
+
+/*_______________________________*/
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Java
 Java Basic Notes and Some Apps
+
+## Notes
+
+  * General Notes [GO](https://github.com/HopeMashal/Java/blob/master/Notes/lesson_1.java)

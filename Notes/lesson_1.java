8ccedaf5a50af7665e0f7b952d98b820c7e95d64
HopeMashal <mashal.hope@gmail.com>
2022-07-11 02:09:33 +0300

    add comments to lesson_1 file

--- Notes/lesson_1.java
+++ Notes/lesson_1.java
@@ -2,7 +2,7 @@ package Notes; //You need to put folder name like this
 
 // You need to put your code into a { CLASS }
 public class lesson_1 {
-  public static void main(String[] args){
+  public static void main(String[] args){ //MAIN method is so important to start the code
     System.out.println("Welcome to Java!!");
   }
 }

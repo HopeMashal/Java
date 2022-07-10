9db82b820d7e75e07195337eb4c9328999d45d54
HopeMashal <mashal.hope@gmail.com>
2022-07-11 02:03:32 +0300

    add lesson_1 file

---
+++ Notes/lesson_1.java
@@ -0,0 +1,8 @@
+package Notes; //You need to put folder name like this
+
+// You need to put your code into a { CLASS }
+public class lesson_1 {
+  public static void main(String[] args){
+    System.out.println("Welcome to Java!!");
+  }
+}

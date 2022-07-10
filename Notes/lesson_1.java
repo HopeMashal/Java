85607680b76b3426c7306b3f8140fd2665883df5
HopeMashal <mashal.hope@gmail.com>
2022-07-11 02:24:03 +0300

    edit lesson 1

--- Notes/lesson_1.java
+++ Notes/lesson_1.java
@@ -9,12 +9,14 @@ multiline comments
 
 /*_______________________________*/
 
+//? To print Something using JAVA
+
 package Notes; //You need to put folder name like this
 
 // You need to put your code into a { CLASS }
 public class lesson_1 {
   public static void main(String[] args){ //MAIN method is so important to start the code
-    //To print something use System.out.println("Your_Msg");
+    //To print something use --> { System.out.println("Your_Msg"); }
     System.out.println("Welcome to Java!!"); 
   }
 }

a3749104b7bfcd60414cf620bf1e838c8e837ff7
HopeMashal <mashal.hope@gmail.com>
2022-07-12 16:27:57 +0300

    edit calculator file

--- Apps/calculator.java
+++ Apps/calculator.java
@@ -5,7 +5,27 @@ import java.util.Scanner;
 
 public class calculator {
   public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
 
+    System.out.print("Enter the first integer: ");
+    float number1 = input.nextFloat(); // read first number from user
+
+    System.out.print("Enter the second integer: ");
+    float number2 = input.nextFloat(); // read second number from user
+
+    System.out.println("\n ____________________________________");
+    System.out.println("|  Please enter the operator number  |");
+    System.out.println("|        Enter 1 for sum (+)         |");
+    System.out.println("|        Enter 2 for sub (-)         |");
+    System.out.println("|        Enter 3 for div (/)         |");
+    System.out.println("|        Enter 4 for mul (*)         |");
+    System.out.println("|        Enter 5 for rod (%)         |");
+    System.out.println("|        Enter 6 for pow (^)         |");
+    System.out.println("|____________________________________|\n");
+    System.out.print("Enter the operator: ");
+    int operator = input.nextInt(); // read operator from user
+
+    
 
   }
 }

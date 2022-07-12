d147474d7d930313d5619baf44ceb7aeb1e88bf9
HopeMashal <mashal.hope@gmail.com>
2022-07-12 22:04:05 +0300

    edit lesson 4 file

--- Notes/lesson_4.java
+++ Notes/lesson_4.java
@@ -4,6 +4,11 @@
 package Notes;
 
 public class lesson_4 {
+  public static void main(String[] args){
+    System.out.println(2+3*5); //* The output is 17
+    System.out.println((2+3)*5); //* The output is 25
+    System.out.println(2*5*5+3*5+7); //* The output os 72 --> (10*5+15+7 = 50+22 = 72)
+  }
   
 }
 
@@ -19,7 +24,7 @@ public class lesson_4 {
 
 /*_______________________________*/
 
-//! Precedence of arithmetic operations
+//! Precedence of arithmetic operations (--> L to R)
 
 //? 1- Parentheses ()
 //? 2- *,/,%

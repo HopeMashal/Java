ed39b9fd4f6a3efad787afae9f377c405ce367d7
HopeMashal <mashal.hope@gmail.com>
2022-07-14 18:36:05 +0300

    edit lesson 6

--- Notes/lesson_6.java
+++ Notes/lesson_6.java
@@ -2,9 +2,34 @@
 //! Switch statement!!
 
 package Notes;
+import java.util.Scanner;
 
 public class lesson_6 {
   public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter Your Grade: ");
+    char grade = input.next().charAt(0);
+
+    switch (grade){
+      case 'A':
+        System.out.println("Excellent");
+        break;
+      case 'B':
+        System.out.println("Very Good");
+        break;
+      case 'C':
+        System.out.println("Good");
+        break;
+      case 'D':
+        System.out.println("Fair");
+        break;
+      case 'F':
+        System.out.println("Failed");
+        break;
+      default:
+        System.out.println("Invalid Grade!!!");
+    }
+    
 
   }
 }

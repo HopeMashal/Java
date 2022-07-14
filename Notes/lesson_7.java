d3999596833a82bc7f7eaee77ace432f33e5984e
HopeMashal <mashal.hope@gmail.com>
2022-07-14 21:28:40 +0300

    edit lesson 7

--- Notes/lesson_7.java
+++ Notes/lesson_7.java
@@ -2,9 +2,11 @@
 //! While Loop!!
 
 package Notes;
+import java.util.Scanner;
 
 public class lesson_7 {
   public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
 
     int count = 0;
     while (count < 5){
@@ -12,7 +14,15 @@ public class lesson_7 {
       count++;
     }
 
-    
+    //! Average grade for 6 students
+    int grade=0, sum=0, counter=1;
+    while(counter<=6){
+      System.out.printf("Enter grade for student no %d : ",counter);
+      grade = input.nextInt();
+      sum += grade;
+      counter++;
+    }
+    System.out.printf("Average Grade is %.2f",(float)sum/6);
 
   }
 }

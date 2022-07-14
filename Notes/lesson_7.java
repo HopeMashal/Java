e0f68e83e54fadf1dc7a60c0c907f920b84cee6a
HopeMashal <mashal.hope@gmail.com>
2022-07-14 21:53:06 +0300

    edit lesson 7 file

--- Notes/lesson_7.java
+++ Notes/lesson_7.java
@@ -22,7 +22,30 @@ public class lesson_7 {
       sum += grade;
       counter++;
     }
-    System.out.printf("Average Grade is %.2f",(float)sum/6);
+    System.out.printf("Average Grade is %.2f %n",(float)sum/6);
+
+    //Average grade for 5 students or ends the program by entering -1
+    int myGrade=0, mySum=0, Counter=1;
+    System.out.println("Enter positive number to get average grade for 5 students or -1 To Exit");
+    System.out.print("Enter the value: ");
+    myGrade = input.nextInt();
+    while(Counter<=5 && myGrade !=-1){
+      System.out.printf("Enter grade for student no %d : ",Counter);
+      myGrade = input.nextInt();
+      mySum += myGrade;
+      if (myGrade == -1){
+        mySum -= myGrade;
+        Counter -= 1;
+        System.out.printf("Average Grade is %.2f for %d students",(float)mySum/Counter,Counter);
+      } else if (Counter == 5){
+        System.out.printf("Average Grade is %.2f for 5 students",(float)mySum/5);
+      }
+      Counter++;
+    }
+
+    if (myGrade == -1 && Counter ==1){
+      System.out.println("EXIT!!");
+    }
 
   }
 }

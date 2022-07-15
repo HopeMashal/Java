34c19007a2ceaf35ba27cfd6d2c706dc524df5cd
HopeMashal <mashal.hope@gmail.com>
2022-07-15 20:36:39 +0300

    edit lesson 8 file

--- Notes/lesson_8.java
+++ Notes/lesson_8.java
@@ -8,6 +8,21 @@ public class lesson_8 {
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
 
+    //! Add positive numbers, To stop enter negative number
+    System.out.println("Enter some positive numbers to add them together or enter negative number to stop the program: ");
+    double sum =0.0; 
+    float value;
+    boolean nonNegative = true;
+    while(nonNegative){
+      value = input.nextFloat();
+      if (value < 0){
+        nonNegative=false;
+      } else {
+        sum += value;
+      }
+    }
+    System.out.printf("Sum of input numbers is %.2f",sum);
+
 
   }
 }

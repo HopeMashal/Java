fd4bf7f13e3657535620e1c8357538e420b1d9a7
HopeMashal <mashal.hope@gmail.com>
2022-07-17 18:51:47 +0300

    add comment in lesson 13 file

--- Notes/lesson_13.java
+++ Notes/lesson_13.java
@@ -10,6 +10,15 @@ public class lesson_13 {
     System.out.println("Welcome to the First Lecture in Functions!!");
     drawLine();
     System.out.println("End of program");
+
+    /* 
+     * The output is:
+          Start of program
+          **********
+          Welcome to the First Lecture in Functions!!
+          **********
+          End of program
+     */
   }
   public static void drawLine(){
     for(int i=0;i<10;i++){

0a9a53d26e2578d0ce46d1d6478dfc37227340f6
HopeMashal <mashal.hope@gmail.com>
2022-07-17 19:06:07 +0300

    edit lesson 13 file

--- Notes/lesson_13.java
+++ Notes/lesson_13.java
@@ -2,13 +2,16 @@
 //! Methods with Parameters!!
 
 package Notes;
+import java.util.Scanner;;
 
 public class lesson_13 {
   public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+
     System.out.println("Start of program");
-    drawLine();
+    drawLine('*',10);
     System.out.println("Welcome to the First Lecture in Functions!!");
-    drawLine();
+    drawLine('-',15);
     System.out.println("End of program");
 
     /* 
@@ -16,15 +19,30 @@ public class lesson_13 {
           Start of program
           **********
           Welcome to the First Lecture in Functions!!
-          **********
+          ---------------
           End of program
      */
+
+     System.out.print("Enter the first number: ");
+     int x=input.nextInt();
+
+     System.out.print("Enter the second number: ");
+     int y=input.nextInt();
+
+     int total = sum(x, y);
+     System.out.println("The Sum of "+x+" & "+y+" is equal "+total);
+
+
   }
-  public static void drawLine(){
-    for(int i=0;i<10;i++){
-      System.out.print("*");
+  public static void drawLine(char ch, int length){
+    for(int i=0;i<length;i++){
+      System.out.print(ch);
     }
     System.out.println();
+  }
+  public static int sum (int i, int j){
+    int result=i+j;
+    return  result;
   }
   
 }

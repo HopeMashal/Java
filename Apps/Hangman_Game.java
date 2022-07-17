6941e02fe8581dd898e90ade38c4662a6a35ee3d
HopeMashal <mashal.hope@gmail.com>
2022-07-17 18:38:51 +0300

    hangman done

--- Apps/Hangman_Game.java
+++ Apps/Hangman_Game.java
@@ -28,7 +28,40 @@ public class Hangman_Game {
     System.out.println("You have "+guess_limit+" guesses");
     System.out.println("The word length is: "+word_guess);
 
-
+    while (!word_guess.equals(word_select) && !out_of_guesses){
+      if (guess_count < guess_limit){
+        System.out.print("Enter Guess: ");
+        char guess_input = input.next().charAt(0);
+        char guess = Character.toLowerCase(guess_input);
+        for (int i = 0; i < word_select.length(); i++){
+          if (guess == word_select.charAt(i)){
+            if (i == 0){
+              word_guess = guess + word_guess.substring(1);
+            }
+            else if (i > 0 && i < (word_guess.length()-1)){
+              word_guess = word_guess.substring(0,i)+ guess +word_guess.substring(i+1) ;
+            } else {
+              word_guess = word_guess.substring(0,word_guess.length()-1)+ guess;
+            }
+            System.out.println("The word is: "+word_guess);
+          }
+        }
+        if (word_select.indexOf(guess) == -1) {
+          guess_count += 1;
+          System.out.println("The word is: "+word_guess);
+          System.out.println("You have "+(guess_limit-guess_count)+" guesses");
+        }
+      } else {
+        out_of_guesses = true;
+      }
+    }
+    if (out_of_guesses){
+      System.out.println("YOU LOSE OUT OF GUESSES!!!");
+      System.out.println("The correct answer is: "+word_select);
+    } else {
+      System.out.println("YOU WIN!!!");
+      System.out.println("The programming language is: "+word_select);
+    }
   }
   
 }

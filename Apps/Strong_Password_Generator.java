cf09ce68c897ae5ea49de6677018b9601221919a
HopeMashal <mashal.hope@gmail.com>
2022-07-19 02:44:45 +0300

    def. variables in spg file

--- Apps/Strong_Password_Generator.java
+++ Apps/Strong_Password_Generator.java
@@ -8,6 +8,11 @@ public class Strong_Password_Generator {
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
 
+    char[] s1 = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    char[] s2 = new char[]{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    int[] s3 = new int[]{1,2,3,4,5,6,7,8,9,0};
+    char[] s4 = new char[]{'~','!','@','#','$','%','^','&','*','(',')','-','_','+','=','[',']','{','}',':',';','"','<','>',',','.','/','?','|'};
+
 
   }
   

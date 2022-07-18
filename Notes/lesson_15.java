bb020144820fa7b34b8c00d5afa2a154dc270425
HopeMashal <mashal.hope@gmail.com>
2022-07-18 15:08:21 +0300

    edit lesson 15 file

--- Notes/lesson_15.java
+++ Notes/lesson_15.java
@@ -14,3 +14,27 @@ public class lesson_15 {
 }
 
 /*_______________________________*/
+
+//! Method Overloading:
+
+//? Defining several methods within a class with the same name. As lond as every method has a different signature
+
+//? Examples:
+/* 
+ * public int Sum(int x, int y);
+ * public int Sum(int x, int y,int z);
+ * public double Sum(double x,double y,double z);
+ * public int Sum(int x, double y);
+ * public int Sum(double x,int y);
+ */
+
+/*_______________________________*/
+
+//! Method Signature:
+
+//? The signature of a method consists of the following:
+//* Method name
+//* Formal Parameter list
+//? Note that the method type is not part of its signature
+
+/*_______________________________*/
